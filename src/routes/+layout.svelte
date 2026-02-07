193af512c776ffb8c659bf038ae7bc40d58f0528
Pauloarf <pauloferreira.vnc@gmail.com>
2026-02-07 22:55:24 +0000

    Fix(linkIcon): Fixes google icon not appearing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="logoCores.webp" type="image/webp" >
     <!-- Canonical URL -->
     <link rel="canonical" href={page.url.href} />
     
