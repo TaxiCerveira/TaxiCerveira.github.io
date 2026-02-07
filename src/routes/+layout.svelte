b124b991240c0aa40c604394746a4efefd7d28c4
Pauloarf <pauloferreira.vnc@gmail.com>
2026-02-07 23:24:58 +0000

    Fix(favicon): Changes it to a more adequade size

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import { locales, localizeHref } from '$lib/paraglide/runtime';
-	import favicon from '$lib/assets/logoCores.webp';
+	import favicon from '$lib/assets/logoCoresIco.webp';
 	import './layout.css';
 
 	let { children } = $props();
